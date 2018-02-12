bb1f264c8cb8856ac2dc17f1d52066475a60448f
Jonas Pohlmann <jonaspohlmann@yahoo.de>
2018-02-12 17:23:17 +0100

    Tested matching cases first parameter, last parameter and no parameter

--- src/test/java/de/hpi/matching/model/MatchingTest.java
+++ src/test/java/de/hpi/matching/model/MatchingTest.java
@@ -3,30 +3,107 @@ package de.hpi.matching.model;
 import de.hpi.matching.repo.OfferMatchingRepository;
 import de.hpi.restclient.clients.BPBridgeClient;
 import de.hpi.restclient.dto.ParsedOffer;
+import de.hpi.restclient.pojo.MatchingResponse;
 import de.hpi.restclient.pojo.Offer;
+import de.hpi.restclient.pojo.SuccessfulMatchingResponse;
+import de.hpi.restclient.pojo.UnsuccessfulMatchingResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.*;
 
 class MatchingTest {
 
+    
+
     @Test
-    void match() {
-        OfferMatchingRepository repository = spy(new OfferMatchingRepository(mock(BPBridgeClient.class)));
+    public void matchFirstParameterEqual () {
 
-        Matching match = new Matching(repository);
-        
+        OfferMatchingRepository repository = mock(OfferMatchingRepository.class);
 
-        ParsedOffer offer = constructParsedOffer();
-        //TODO Test different strategies
+        Matching matcher = new Matching(repository);
+
+        List<Offer> testOffers= new ArrayList<Offer>();
+        testOffers.add(constructIdealoOffer());
+
+        List<Offer> emptyList = new ArrayList<Offer>();
+
+        when(repository.searchEan(anyLong(), anyString())).thenReturn(testOffers);
+
+        when(repository.searchBrand(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchHan(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchOfferTitle(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchSku(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchCategory(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchDescription(anyLong(), anyString())).thenReturn(emptyList);
 
 
+        MatchingResponse response = matcher.match(constructParsedOffer());
+
+        assertTrue(response instanceof SuccessfulMatchingResponse);
     }
+
+    @Test
+    public void matchLastParameterEqual () {
+
+        OfferMatchingRepository repository = mock(OfferMatchingRepository.class);
+
+        Matching matcher = new Matching(repository);
+
+        List<Offer> testOffers= new ArrayList<Offer>();
+        testOffers.add(constructIdealoOffer());
+        List<Offer> emptyList = new ArrayList<Offer>();
+
+        when(repository.searchOfferTitle(anyLong(), anyString())).thenReturn(testOffers);
+
+        when(repository.searchEan(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchBrand(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchHan(anyLong(), anyString())).thenReturn(emptyList);
+
+        when(repository.searchSku(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchCategory(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchDescription(anyLong(), anyString())).thenReturn(emptyList);
+
+
+        MatchingResponse response = matcher.match(constructParsedOffer());
+
+        assertTrue(response instanceof SuccessfulMatchingResponse);
+    }
+
+    @Test
+    public void matchNoParameterEqual () {
+
+        OfferMatchingRepository repository = mock(OfferMatchingRepository.class);
+
+        Matching matcher = new Matching(repository);
+
+        List<Offer> testOffers= new ArrayList<Offer>();
+        testOffers.add(constructIdealoOffer());
+        List<Offer> emptyList = new ArrayList<Offer>();
+
+        when(repository.searchOfferTitle(anyLong(), anyString())).thenReturn(emptyList);
+
+        when(repository.searchEan(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchBrand(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchHan(anyLong(), anyString())).thenReturn(emptyList);
+
+        when(repository.searchSku(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchCategory(anyLong(), anyString())).thenReturn(emptyList);
+        when(repository.searchDescription(anyLong(), anyString())).thenReturn(emptyList);
+
+        MatchingResponse response = matcher.match(constructParsedOffer());
+
+        assertTrue(response instanceof UnsuccessfulMatchingResponse);
+    }
+
+
 
     private ParsedOffer constructParsedOffer() {
         return constructParsedOfferParameter(
@@ -35,7 +112,9 @@ class MatchingTest {
             "Dein Gesicht ist jetzt dein Passwort",
             "iPhone 7 32GB white",
             "123456",
-            "777888999" );
+            "777888999",
+            "654321"
+        );
     }
 
     private Offer constructIdealoOffer(){
@@ -45,10 +124,12 @@ class MatchingTest {
             "Dein Gesicht ist jetzt dein Passwort",
             "iPhone 7 32GB white",
             "123456",
-            "777888999" );
+            "777888999",
+            "65432881"
+        );
     }
 
-    private ParsedOffer constructParsedOfferParameter(String brand, String categoryString, String description, String offerTitle, String sku, String han) {
+    private ParsedOffer constructParsedOfferParameter(String brand, String categoryString, String description, String offerTitle, String sku, String han, String ean) {
         ParsedOffer testOffer = new ParsedOffer();
         testOffer.setBrand(brand);
         testOffer.setCategoryString(categoryString);
@@ -56,10 +137,11 @@ class MatchingTest {
         testOffer.setSku(sku);
         testOffer.setOfferTitle(offerTitle);
         testOffer.setHan(han);
+        testOffer.setEan(ean);
         return testOffer;
     }
 
-    private Offer constructOfferParameter(String brand, String categoryString, String description, String offerTitle, String sku, String han){
+    private Offer constructOfferParameter(String brand, String categoryString, String description, String offerTitle, String sku, String han, String ean){
         Offer testOffer = new Offer();
 
         Map<String, String> descriptionMap = new HashMap<>();
@@ -67,7 +149,7 @@ class MatchingTest {
         testOffer.setDescription(descriptionMap);
 
         Map<String, String> titleMap = new HashMap<>();
-        descriptionMap.put("0", offerTitle);
+        titleMap.put("0", offerTitle);
         testOffer.setOfferTitle(titleMap);
 
         testOffer.setBrandSearchtext(brand);
@@ -75,6 +157,7 @@ class MatchingTest {
         testOffer.setDescription(descriptionMap);
         testOffer.setHan(han);
         testOffer.setSku(sku);
+        testOffer.setEan(ean);
         return testOffer;
     }
 }
