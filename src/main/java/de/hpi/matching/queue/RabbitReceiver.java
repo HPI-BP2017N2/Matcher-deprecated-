12718070ef2e20a2c947c488a81060182d5f5a2e
dey4ss <lindnerdaniel@yahoo.de>
2018-02-19 17:47:40 +0100

    changed message receiving object

--- src/main/java/de/hpi/matching/queue/RabbitReceiver.java
+++ src/main/java/de/hpi/matching/queue/RabbitReceiver.java
@@ -6,13 +6,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class RabbitReceiver {
@@ -25,20 +20,8 @@ public class RabbitReceiver {
         setController(controller);
     }
 
-    public void receiveMessage(String message) {
+    public void receiveMessage(FinishedShop message) {
         getLogger().info("Received <" + message + "> from queue");
-        FinishedShop shop = null;
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            shop = mapper.readValue(message, FinishedShop.class);
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if(shop != null) getController().startMatchingOffersForShop(shop.getShopId());
+        getController().startMatchingOffersForShop(message.getShopId());
     }
 }
